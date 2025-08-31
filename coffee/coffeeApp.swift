48a52d9bdf896a4c20887c62c1b5147337668c43
Svetlana <svetzayats@gmail.com>
2025-08-31 14:18:08 -0400

    Initial Commit

---
+++ coffee/coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeApp.swift
+//  coffee
+//
+//  Created by Svetlana Zayats on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
