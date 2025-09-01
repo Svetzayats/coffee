fb034c9257be0d440eadbcae348b78eb04dd0798
Svetlana <svetzayats@gmail.com>
2025-09-01 19:26:15 -0400

    Add filter (search by name)

--- coffee/PageViews/MenuPage.swift
+++ coffee/PageViews/MenuPage.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct MenuPage: View {
     @EnvironmentObject var menuManager: MenuManager
+    @State var search: String = ""
     
     var body: some View {
         NavigationView {
             List {
                 ForEach (menuManager.menu) { category in
-                    Text(category.name)
-                    ForEach (category.products) {
-                        product in
-                        NavigationLink {
-                            DetailsPage(product: product)
-                        } label: {
-                            ProductItem(product: product)
+                    if category.filteredItems(text: search).count > 0 {
+                            Text(category.name)
+                            .listRowBackground(Color("Background"))
+                            .foregroundColor(Color("Secondary"))
+                            .padding()
                         }
-                    }
+                    ForEach(category.filteredItems(text: search)) { item in
+                            ZStack {
+                                NavigationLink(destination: DetailsPage(product: item)) {
+                                }.opacity(0)
+                                ProductItem(product: item)
+                                    .padding(.top)
+                                    .padding(.leading)
+                                    .padding(.bottom, 12)
+
+                            }
+                            
+                        }
                 }
             }.navigationTitle("Products")
+                .searchable(text: $search)
         }
     }
 }
